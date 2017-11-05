bf21ed3d1bd01095c33f7e3c0edf7b8c23693546
Administrator <Administrator@User-PC>
2017-11-05 19:25:25 +0000

    First Commit

---
+++ src/main/java/com/javawork/practice/corejava/client/CoreJavaClient.java
@@ -0,0 +1,9 @@
+package com.javawork.practice.corejava.client;
+
+public class CoreJavaClient {
+
+	public static void main(String[] args) {
+		System.out.println("EXECUTING CORE JAVA CLIENT");
+	}
+
+}
