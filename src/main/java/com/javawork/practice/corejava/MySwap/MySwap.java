0c613666a1103997dac964949abac7229f35455e
Administrator <Administrator@User-PC>
2017-11-06 10:55:42 +0000

    creating class and interface

---
+++ src/main/java/com/javawork/practice/corejava/MySwap/MySwap.java
@@ -0,0 +1,23 @@
+package com.javawork.practice.corejava.MySwap;
+
+public class MySwap implements ISwap{
+	
+	public int bswap(int a)
+	{
+		return a;
+	}
+	public int bswap1(int b)
+	{
+		return b;
+	}
+	public int aswap(int c)
+	{
+		return c;
+	}
+	public int aswap1(int d)
+	{
+		return d;
+	}
+
+	
+}
--- src/main/java/com/javawork/practice/corejava/client/CoreJavaClient.java
+++ src/main/java/com/javawork/practice/corejava/client/CoreJavaClient.java
@@ -1,9 +1,33 @@
 package com.javawork.practice.corejava.client;
 
+import com.javawork.practice.corejava.MySwap.ISwap;
+import com.javawork.practice.corejava.MyWordPrint.IWordPrint;
+import com.javawork.practice.corejava.MyWordPrint.MyWordPrint;
+
 public class CoreJavaClient {
 
 	public static void main(String[] args) {
 		System.out.println("EXECUTING CORE JAVA CLIENT");
+		/*IWordPrint wp = new MyWordPrint()
+		{
+			String word = wp.print("HELLO");
+			String word1 = wp.print1("Alexandra Abramov");
+		
+		}*/
+		
+		ISwap swap = new MySwap()
+		{
+			int a = swap.bswap(10);
+			int b = swap.bswap1(20);
+			//System.out.println("VALUE BEFORE SWAP ");
+			System.out.println("A ::"+a);
+			System.out.println("B ::"+b);
+			int a = swap.bswap(a+b);
+			int b = swap.bswap1(a-b);		
+			System.out.println("VALUE AFTER SWAP ");
+			System.out.println("A ::"+a);
+			System.out.println("B ::"+b);
+		}
 	}
 
 }
